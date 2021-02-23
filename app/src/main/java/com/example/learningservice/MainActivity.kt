42ac8a63d25443f089f0e8175e21254278ac59f4
Kshitij Kumar <kshitij2212@gmail.com>
2021-02-23 08:45:13 +0530

    made a service started and bound

--- app/src/main/java/com/example/learningservice/MainActivity.kt
+++ app/src/main/java/com/example/learningservice/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.learningservice
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.widget.Toast
 import com.example.learningservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var service : ServiceClass
+    private var isBounded = false
+    private var isServiceStarted = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as ServiceClass.MyServiceBinder
+            this@MainActivity.service = binder.getService()
+            isBounded = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBounded = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +36,42 @@ class MainActivity : AppCompatActivity() {
         binding.btnStartService.setOnClickListener {
             val intent = Intent(this, ServiceClass::class.java)
             intent.putExtra("name", "Kshitij")
+            isServiceStarted = true
             startService(intent)
+            bindToService()
         }
 
         binding.btnStopService.setOnClickListener {
+            isBounded = false
+            unbindService(connection)
             stopService(Intent(this, ServiceClass::class.java))
+            isServiceStarted = false
+        }
+
+        binding.btnFetch.setOnClickListener {
+            if(isBounded){
+                Toast.makeText(this, "The random num fetched is ${service.sendRandomNum()}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        if(isServiceStarted){
+            bindToService()
+        }
+    }
+
+    private fun bindToService() {
+        val intent = Intent(this, ServiceClass::class.java)
+        bindService(intent, connection, BIND_AUTO_CREATE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        isBounded = false
+    }
+
+
 }
--- app/src/main/java/com/example/learningservice/ServiceClass.kt
+++ app/src/main/java/com/example/learningservice/ServiceClass.kt
@@ -2,22 +2,29 @@ package com.example.learningservice
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
+import java.util.Random
 
 class ServiceClass : Service() {
 
-    var job: Job? = null
+    private var job: Job? = null
+    private val binder = MyServiceBinder()
 
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        return binder
     }
 
     override fun onCreate() {
         super.onCreate()
         Log.d("ServiceClass", "Service onCreate")
     }
+
+    private val randomGenerator = Random()
+
+    fun sendRandomNum() = randomGenerator.nextInt(100)
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("ServiceClass", "Service onStartCommand")
@@ -26,16 +33,20 @@ class ServiceClass : Service() {
         }
 
         job = CoroutineScope(Dispatchers.Main).launch {
-            for(i in 0..10){
+            for(i in 0..60){
                 Log.d("ServiceClass", "Running at $i")
                 delay(1000L)
-                if(i == 10){
+                if(i == 60){
                     stopSelf()
                 }
             }
         }
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    inner class MyServiceBinder : Binder() {
+        fun getService() = this@ServiceClass
     }
 
     override fun onDestroy() {
